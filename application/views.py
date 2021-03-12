85bba9161fb5f98bb5dcf9eb8d42101fc7936981
amnong21 <amnong21@gmail.com>
2021-03-13 01:43:05 +0200

    html

--- application/views.py
+++ application/views.py
@@ -1,8 +1,11 @@
 from application import application
+from flask import render_template, request, redirect, url_for, abort, send_from_directory, jsonify, make_response
+from werkzeug.utils import secure_filename
+import os
 
 @application.route('/')
 def hello_world():
-    return 'Hello world1!'
+    return render_template("/index.html")
 
 @application.route('/about/')
 def about():
