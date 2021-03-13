5b2db934e6647949908115ba983912ba912741af
amnong21 <amnong21@gmail.com>
2021-03-13 02:34:27 +0200

    upload_with_S3

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_s3 import FlaskS3
 
 application = Flask(__name__)
+
+application.config['FLASKS3_BUCKET_NAME'] = 'AmnonBucket'
+s3 = FlaskS3(application)
 
 print(application.config['ENV'])
 if application.config["ENV"] == "production":
--- config.py
+++ config.py
@@ -1,13 +1,20 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
 
     SESSION_COOKIE_SECURE = True
 
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    ACL = 'public-read'
+    FLASKS3_BUCKET_NAME = os.environ.get('FLASKS3_BUCKET_NAME')
+    FLASKS3_REGION = os.environ.get('FLASKS3_REGION') 
+
     UPLOAD_EXTENSIONS = ['XLSX', 'XLS']
     ALLOWED_IMAGE_EXTENSIONS = ["JPEG", "JPG", "PNG", "GIF"]
 
-    # UPLOAD_PATH = "C:\\Users\\user\\Documents"
     UPLOAD_PATH   = "C:\\Users\\user\\Documents\\Python_Scripts\\Phenome_tools\\server\\uploads\\"
     DOWNLOAD_PATH = "C:\\Users\\user\\Documents\\Python_Scripts\\Phenome_tools\\server\\downloads\\"
 
